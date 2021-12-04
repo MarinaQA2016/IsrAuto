f6df18731a2941bb90d35ce8a8125f5bc0642b1f
MarinaQA2016 <marinaqatest2016@gmail.com>
2021-12-04 10:07:35 +0200

    The method encodingSet() @BeforeSuit was added to the TestBase

--- src/test/java/org/exmpl/tests/TestBase.java
+++ src/test/java/org/exmpl/tests/TestBase.java
@@ -6,12 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.PrintStream;
 
 public class TestBase {
     public static final String LOGIN = "admin";
     public static final String PASSWORD = "hatul";
     WebDriver driver;
     LoginPageHelper loginPage;
+    @BeforeSuite
+    public void encodingSet(){
+        String consoleEncoding = System.getProperty("consoleEncoding");
+        if (consoleEncoding != null) {
+            try {
+                System.setOut(new PrintStream(System.out, true, consoleEncoding));
+            } catch (java.io.UnsupportedEncodingException ex) {
+                System.err.println("Unsupported encoding set for console: "+consoleEncoding);
+            }
+        }
+    }
     @BeforeMethod
     public void startUp(){
         driver = new ChromeDriver();
